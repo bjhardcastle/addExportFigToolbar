03fc3e05fc0385c5a62f2a5397aadab1f0877585
Ben Hardcastle <63425812+bjhardcastle@users.noreply.github.com>
2020-04-23 13:44:17 -0700

    Update addExportFigToolbar.m

--- addExportFigToolbar.m
+++ addExportFigToolbar.m
@@ -1,141 +1,141 @@
 function addExportFigToolbar(fig)
-% Add a toolbar with shortcuts for common plot-related functions, such as
-% exporting to .png or .svg via export_fig or plot2svg.
-% Tool bar appears at the top of the specified figure 'fig'
+%ADDEXPORTFIGTOOLBAR Add a toolbar with shortcuts for exporting a figure in
+%different formats
+%                                                                  BJH 2017
+%
+% If no input argument is provided just use the current figure
 if nargin<1 || isempty(fig)
     fig = gcf;
 end
+
+% Check whether this function has already created a toolbar in this figure
 for idx = 0:length(fig.Children)
     if idx
-        if strcmp(fig.Children(idx).Tag, 'export_fig')
-            % Toolbar already exists: don't draw another 
+        if strcmp(fig.Children(idx).Tag, 'exportFigToolbar')
+            % Toolbar already exists so don't draw another 
            return
         end
     end
 end
-    
-tbar = uitoolbar(fig,'Tag','export_fig');
 
-% Add a button for saving to .png
-saveimg = im2uint8(imread(fullfile(matlabroot,'toolbox/matlab/icons/file_save.png'),'png','BackgroundColor' ,'none'));
+% Create toolbar with an identifying tag
+tbar = uitoolbar(fig,'Tag','exportFigToolbar');
+
+% Fetch a built-in save file icon (blue):
+try
+    saveimg = im2uint8(imread(fullfile(matlabroot,'toolbox/matlab/icons/file_save.png'),'png','BackgroundColor' ,'none'));
+catch
+    % If it's not available just make a soild blue square instead
+    saveimg = cat(3,0.2.*ones(16,16),0.2.*ones(16,16),ones(16,16));
+end
+
+% Make some colorized icons:
+iconBlue = cat(3,saveimg(:,:,2), saveimg(:,:,1), saveimg(:,:,3));
+iconRed = cat(3,saveimg(:,:,3), saveimg(:,:,2), saveimg(:,:,1));
+iconGreen = cat(3,saveimg(:,:,1), saveimg(:,:,3), saveimg(:,:,2));
+iconYellow = cat(3,saveimg(:,:,3), saveimg(:,:,3), saveimg(:,:,2));
+
+% Add toggle buttons to toolbar for saving to each format 
+% Note: Tag should be file extension w/out leading "." -
+% it can contain number identifiers though (png1, png2, etc.) which will be
+% stripped later
+%png
 png_button = uitoggletool(tbar, ...
-    'CData', cat(3,saveimg(:,:,3), saveimg(:,:,2), saveimg(:,:,1)), ... % Colorized red
+    'CData', iconRed, ...
     'TooltipString', 'Export .png', ...
-    'HandleVisibility', 'off', ...
-    'Interruptible', 'off', ...
-    'BusyAction', 'cancel');
-
-% Make a button for saving to .svg
+    'Tag', 'png1' ); %file extension w/optional number
+%svg
 svg_button = uitoggletool(tbar, ...
-    'CData', cat(3,saveimg(:,:,1), saveimg(:,:,3), saveimg(:,:,2)), ... % colorized green
+    'CData', iconGreen, ...
     'TooltipString', 'Export .svg', ...
-    'HandleVisibility', 'off', ...
-    'Interruptible', 'off', ...
-    'BusyAction', 'cancel');
-
-% % Make a button for saving to .eps
-% eps_button = uitoggletool(tbar, ...
-%     'CData', saveimg, ...
-%     'TooltipString', 'Export .eps', ...
-%     'HandleVisibility', 'off', ...
-%     'Interruptible', 'off', ...
-%     'BusyAction', 'cancel');
-
-% Make a button for saving to .pdf
+    'Tag', 'svg1' ); %file extension w/optional number
+%pdf
 pdf_button = uitoggletool(tbar, ...
-    'CData', cat(3,saveimg(:,:,2), saveimg(:,:,1), saveimg(:,:,3)), ... % colorized blue
+    'CData', iconBlue, ... 
     'TooltipString', 'Export .pdf', ...
+    'Tag', 'pdf1' ); %file extension w/optional number
+
+%{ 
+%eps
+eps_button = uitoggletool(tbar, ...
+    'CData', iconYellow, ... 
+    'TooltipString', 'Export .eps', ...
+    'Tag', 'eps1' ); %file extension w/optional number
+%}
+    
+% Set a common callback for all buttons (which is where customized
+% export settings should be added)
+set( [...
+    png_button, ...
+    svg_button, ...
+    pdf_button ...
+    ], ...
+    'ClickedCallback', @(src, evt) buttonCallback(fig, src, evt),...
     'HandleVisibility', 'off', ...
     'Interruptible', 'off', ...
-    'BusyAction', 'cancel');
+    'BusyAction', 'cancel' );
 
-udata.png_button = png_button;
-udata.svg_button = svg_button;
-% udata.eps_button = eps_button;
-udata.pdf_button = pdf_button;
+% Assign buttons to gui's user data so we can retrieve it in callback function:
+udata.([png_button.Tag '_button']) = png_button;
+udata.([svg_button.Tag '_button']) = svg_button;
+udata.([pdf_button.Tag '_button']) = pdf_button;
+% udata.([eps_button.Tag 'button']) = eps_button;
 
+% Set the userdata
 set(fig, 'UserData', udata);
 
-
-% Set callbacks
-set(png_button, ...
-    'ClickedCallback', @(src, evt) pngCallback(fig, src, evt));
-set(svg_button, ...
-    'ClickedCallback', @(src, evt) svgCallback(fig, src, evt));
-% set(eps_button, ...
-%     'ClickedCallback', @(src, evt) epsCallback(fig, src, evt));
-set(pdf_button, ...
-    'ClickedCallback', @(src, evt) pdfCallback(fig, src, evt));
 end
 
-function pngCallback(fig,~,~)
+function buttonCallback(fig,src,~)
+% Customize export settings for shortcuts
+
+% Fetch figure user data
 udata = get(fig,'UserData');
 
-[filename, pathname] = uiputfile('.png','Save image as...', char(datetime('now','Format','yyMMddHHmmss')) );
+% Get save location (defaults to what should be a unique filename)
+fileExt = src.Tag(isletter(src.Tag));
+[filename, pathname] = uiputfile(['.',fileExt],'Save image as...', char(datetime('now','Format','yyMMddHHmmss')) );
+assert(ischar(filename),'Bad filename type. Should be a string of characters')
 
-if ischar( filename )
+% Get current figure color in case we change it:
+originalColor = get(fig,'color');
+
+% Apply some common settings for all formats:
+%set(fig, 'InvertHardCopy', 'off'); % keeps fig/ax background colors displayed on screen
     
-    % Old method using export_fig (started giving crap results)
-    %     export_fig( fullfile(pathname,filename), ...
-    %         '-png', '-m4', fig);
+    switch src.Tag
+        % Export with desired settings for each format
+        
+        case 'png1'
+            % Transparent background with export_fig:
+            %{
+            set(fig,'color','none');
+            export_fig(fullSavePath , '-dpng','-r400','-q100','-opengl',gcf);
+            %}
+            print(fig, fullfile(pathname,filename) , '-dpng','-r400');
 
-    % New method with built-in Matlab function
-    set(fig, 'InvertHardCopy', 'on');
-    print(fig, fullfile(pathname,filename) , '-dpng','-r400');
+        case 'svg1'
+            d = msgbox('Hang on, this may take a minute..','Saving .svg','help');
+            print(fig, fullfile(pathname,filename) , '-dsvg','-painters');
+            close(d)
 
+        case 'pdf1'
+            d = msgbox('Hang on, this may take a minute..','Saving .pdf','help');
+            % export_fig(fullSavePath , '-dpdf','-painters','-transparent',gcf);
+            print(fig, fullfile(pathname,filename) , '-dpdf','-painters');
+            close(d)
+
+    end
+
+% Reapply figure color in case it was removed
+if exist('originalColor','var')
+    set(fig,'color',originalColor);
 end
 
-udata.png_button.State = 'off';
-set(fig, 'UserData', udata);
+% Reset button to be used again
+udata.([src.Tag '_button']).State = 'off';
 
-end
-
-function svgCallback(fig,~,~)
-udata = get(fig,'UserData');
-
-[filename, pathname] = uiputfile('.svg','Save image as...', char(datetime('now','Format','yyMMddHHmmss')) );
-if ischar( filename )
-    d = msgbox('Hang on, this may take a minute..','Saving .svg','help');
-    set(fig, 'InvertHardCopy', 'off');
-    print(fig, fullfile(pathname,filename) , '-dsvg','-painters');
-    close(d)
-end
-
-udata.svg_button.State = 'off';
-set(fig, 'UserData', udata);
-
-end
-
-function epsCallback(fig,~,~)
-udata = get(fig,'UserData');
-
-[filename, pathname] = uiputfile('.eps','Save image as...', char(datetime('now','Format','yyMMddHHmmss')) );
-if ischar( filename )
-    d = msgbox('Hang on, this may take a minute..','Saving .eps','help');
-    set(fig, 'InvertHardCopy', 'off');
-     print(fig, fullfile(pathname,filename) , '-depsc','-painters');
-    epsclean(fullfile(pathname,filename));
-    close(d)
-end
-
-udata.eps_button.State = 'off';
-set(fig, 'UserData', udata);
-
-end
-
-function pdfCallback(fig,~,~)
-udata = get(fig,'UserData');
-
-[filename, pathname] = uiputfile('.pdf','Save image as...', char(datetime('now','Format','yyMMddHHmmss')) );
-if ischar( filename )
-    d = msgbox('Hang on, this may take a minute..','Saving .pdf','help');
-    set(fig, 'InvertHardCopy', 'off');
-    set(fig,'color','none');
-    print(fig, fullfile(pathname,filename) , '-dpdf','-painters');
-    close(d)
-end
-
-udata.pdf_button.State = 'off';
+% Update figure user data
 set(fig, 'UserData', udata);
 
 end
